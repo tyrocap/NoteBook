4d0f74d66f423396c060f2927ce5d1ce6facca1d
adam <asoliyev13@gmail.com>
2020-05-05 11:45:00 -0400

    Added several forms and Updated urls.py

---
+++ BOOK/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import BookListView, NoteAddPage, Book_Add_Page, search_new_book, note_add_book
+
+urlpatterns = [
+    path('', BookListView.as_view(), name='home_page'),
+    path('notes/<uuid:pk>', NoteAddPage.as_view(), name='notes_page'),
+    path('book/addition/', Book_Add_Page.as_view(), name='book_add_page'),
+    path('search_book/', search_new_book, name='search_new_book'),
+    path('note_add_book/', note_add_book, name='note_add_book'),
+
+]
