aaed2f1a3f207c7744b72623a7b9e4586774fa34
adam <asoliyev13@gmail.com>
2020-05-05 22:12:55 -0400

    Updated README.MD and urls.py

--- BOOK/urls.py
+++ BOOK/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import BookListView, NoteAddPage, Book_Add_Page, search_new_book, note_add_book
+from .views import BookListView, NoteAddPage, Book_Add_Page, search_new_book,\
+    note_add_book, add_new_book
 
 urlpatterns = [
     path('', BookListView.as_view(), name='home_page'),
@@ -7,5 +8,5 @@ urlpatterns = [
     path('book/addition/', Book_Add_Page.as_view(), name='book_add_page'),
     path('search_book/', search_new_book, name='search_new_book'),
     path('note_add_book/', note_add_book, name='note_add_book'),
-
+    path('add_new_book/', add_new_book, name='add_new_book'),
 ]
