fa5e17f347ff544569bb1a221fe942543d847bf1
adam <asoliyev13@gmail.com>
2020-05-08 14:23:39 -0400

    Updated Settings

--- BOOK_FINDER_V1/settings.py
+++ BOOK_FINDER_V1/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9!-xemx5j^d3ee*it8dp%_&t#i$di5qi7b&%z5x_h6crl-2est'
+SECRET_KEY = os.environ.get('DJANGO_SUMNOTESAPP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sumnotes.herokuapp.com']
 
 
 # Application definition
